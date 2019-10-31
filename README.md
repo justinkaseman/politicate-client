117f36a16db60adb0cb1aeea20365386dd486f87
justinkaseman <justinkaseman@live.com>
2019-10-30 21:48:04 -0500

    Init TypeScript

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Politicate Client
+
+### Dependency Documentation
+
+- TypeScript [https://www.typescriptlang.org/docs/home.html]
+- Razzle [https://github.com/jaredpalmer/razzle]
+- React.js [https://reactjs.org/docs/getting-started.html]
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import logo from './react.svg';
-import './Home.css';
+import React from "react";
+import logo from "./react.svg";
 
-class Home extends React.Component {
-  render() {
+import "./Home.css";
+
+class Home extends React.Component<{}, {}> {
+  public render() {
     return (
       <div className="Home">
         <div className="Home-header">
           <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
+          <h2>Welcome to Razzles</h2>
         </div>
         <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> or{" "}
+          <code>src/Home.tsx</code> and save to reload.
         </p>
         <ul className="Home-resources">
           <li>
